ddcb73fe40207304faa9fc58f1449279fe14b414
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-09 17:10:36 +0530

    second commit

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.json("This is home page")
 })
+app.get("/home", (req, res) => {
+    res.json("Home!!!!!!!")
+})
+
 
 app.listen(8082, () => {
     console.log("server created!!!")
