f5b502ead0256b3a9b5a98447a9890d171977986
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-15 21:04:16 +0530

    api 13

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 8082
 
@@ -10,6 +11,12 @@ app.use(express.json())
 app.use(cors())
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`SERVER IS RUNNING ON ${PORT}`)))
+    .catch((error) => console.log(error));
 
 
 app.get("/", (req, res) => {
