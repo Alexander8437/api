471a9483a9ef388f1ee4de636db9851db473d9ee
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-15 19:00:00 +0530

    only api 7

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const cors  = require('cors')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 8082
 
