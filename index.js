227e0cb4f5403aca71a0069c14bfafc11d3579c0
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-15 16:49:22 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-import express from 'express'
-import cors  from 'cors'
-// import ServerlessHttp from 'serverless-http'
-
-// const x  = require('./src/data')
-// const y = require('./src/data')
+const express = require('express')
+const cors  = require('cors')
 
 const PORT = process.env.PORT || 8082
 
@@ -11,12 +7,8 @@ const app = express()
 
 const router = express.Router()
 
-// app.use(express.json())
-// app.use(cors())
-// const y  = x
-
-// console.log(x.get)
-// console.log("Y function ", y)
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("This is home page")
