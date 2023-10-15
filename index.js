e37f0164c43e4c15800fdf2efe2d5af34dfaee15
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-15 21:01:50 +0530

    api 12

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
 
 app.get("/", (req, res) => {
     res.send("This is home page")
