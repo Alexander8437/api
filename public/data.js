c67d56c036ddc31630260775e1032a19b8257abc
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-15 16:45:53 +0530

    sixth commit

---
+++ public/data.js
@@ -0,0 +1,15 @@
+const x = () =>{
+    const name = "Alexander";
+    console.log("first")
+    return name
+}
+
+const y = () =>{
+    const age = 20
+    console.log("second")
+    return age
+}
+// module.exports = {x, y};
+// // module.exports = y;
+
+// exports.x = x
--- public/src/app.js
+++ public/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors  from 'cors'
-// import ServerlessHttp from 'serverless-http'
+import ServerlessHttp from 'serverless-http'
 
 // const x  = require('./src/data')
 // const y = require('./src/data')
@@ -18,18 +18,18 @@ const router = express.Router()
 // console.log(x.get)
 // console.log("Y function ", y)
 
-app.get("/", (req, res) => {
-    res.send("This is home page")
+router.get("/", (req, res) => {
+    res.json("This is home page")
 })
 
-app.get("/home", (req, res) => {
+router.get("/home", (req, res) => {
     res.json("Home!!!!!!!")
 })
 
-// app.use("/.netlify/functions/api", router)
+app.use("/.netlify/functions/api", router)
 
-// module.exports.handler = ServerlessHttp(app)
+module.exports.handler = ServerlessHttp(app)
 
 app.listen(PORT, () => {
-    console.log(`server created!!! ${PORT}` )
+    console.log("server created!!!")
 })
