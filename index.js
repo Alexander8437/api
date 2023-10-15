5017f0f272d2669defdcb7a0b8c308c9c7f03ff9
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-15 18:46:51 +0530

    only api 2

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const cors  = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 const PORT = process.env.PORT || 8082
 
 const app = express()
 
 app.use(express.json())
-
 app.use(cors())
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+dotenv.config()
 
 app.get("/", (req, res) => {
     res.send("This is home page")
