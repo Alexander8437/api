85947c37144c1678fe85668dd0664a11f3fc7014
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-15 21:17:42 +0530

    api 15

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-const PORT = process.env.PORT || 8082
 
 const app = express()
 
@@ -11,13 +11,12 @@ app.use(express.json())
 app.use(cors())
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+dotenv.config()
 
+const PORT = process.env.PORT || 8082
 
-const CONNECTION_URL = "mongodb+srv://memories123:memories123@cluster0.aa9gsoh.mongodb.net/?retryWrites=true&w=majority"
-
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`SERVER IS RUNNING ON ${PORT}`)))
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`SERVER IS RUNNING ON ${PORT} and DB connected.` )))
     .catch((error) => console.log(error));
 
 
