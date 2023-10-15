e77dfffb8c53faf2d44d04ffef5f7489bd35f70f
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-15 21:21:05 +0530

    api 16

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const dotenv = require('dotenv')
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000/',
+    credentials: true,
+    optionSuccessStatus: 200
+}))
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 dotenv.config()
@@ -16,7 +20,7 @@ dotenv.config()
 const PORT = process.env.PORT || 8082
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`SERVER IS RUNNING ON ${PORT} and DB connected.` )))
+    .then(() => app.listen(PORT, () => console.log(`SERVER IS RUNNING ON ${PORT} and DB connected.`)))
     .catch((error) => console.log(error));
 
 
